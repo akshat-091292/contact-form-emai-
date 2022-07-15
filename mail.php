f51f6a9a8e99f8e8bf029241a9d5d0172f43af7d
Raushan Kumar <33718703+C0d3-R3d@users.noreply.github.com>
2022-07-15 22:33:33 +0530

    Update
    Have tweaked it into the same old code. 
    Also, try to use the "else" case when you are practicing, it will help you to manually break the code function and understand where it going wrong.

--- mail.php
+++ mail.php
@@ -1,16 +1,42 @@
-<?php
-$name=$_POST['name'];
-$email=$_POST['email'];
-$phone=$_POST['phone'];
-$message=$_POST['message'];
-$to="akshattripathi215@gmail.com";
-$subject="form response";
-$txt="Name = ".$name . "\r\n Email = ".$email. "\r\n Message=".$message ."\r\n Mobile number=".$number;
-$headers="From:noreply@nosir.com"."\r\n" .
-"CC:tripatakshat09@gmail.com";
-if($email!=Null){
-    mail($to,$subject,$txt,$headers);
-}
-header("Location:ad.html");
-?>
+#<?php
+#$name=$_POST['name'];
+#$email=$_POST['email'];
+#$phone=$_POST['phone'];
+#$message=$_POST['message'];
+#$to="akshattripathi215@gmail.com";
+#$subject="form response";
+#$txt="Name = ".$name . "\r\n Email = ".$email. "\r\n Message=".$message ."\r\n Mobile number=".$number;
+#$headers="From:noreply@nosir.com"."\r\n" .
+#"CC:tripatakshat09@gmail.com";
+#if($email!=Null){
+#    mail($to,$subject,$txt,$headers);
+#}
+#header("Location:ad.html");
+#?>
 
+#New contrib
+
+<?PHP
+if(isset($_POST['submit'])){
+   $name=$_POST['name'];
+   $email=$_POST['email'];
+   $phone=$_POST['phone'];
+   $message=$_POST['message'];
+
+   $to="akshattripathi215@gmail.com";
+   $subject="Form Submission";
+   $txt="Name = ".$name . "\r\n Email = ".$email. "\r\n Message=".$message ."\r\n Mobile number=".$number;
+   $headers="From:noreply@nosir.com"."\r\n" .
+   "CC:tripatakshat09@gmail.com";
+   if($email!=Null){
+    mail($to,$subject,$txt,$headers);
+    }
+    header("Location:ad.html");
+    else {
+          echo "Something wrong";
+    }
+}
+
+
+
+?>
